8da6aa296385a145eb4c66aa6e706f5ff9c4ce9f
Gad-Ongoro <gadongoro1@gmail.com>
2023-12-29 19:34:36 +0300

    Clean Up

--- game-store/src/Components/Games.js
+++ game-store/src/Components/Games.js
@@ -1,4 +1,4 @@
-import React, {useRef, useEffect, useState} from "react";
+import React, {useRef, useState} from "react";
 
 function Games({games}){
     const [thumbNail, setThumbNail] = useState(`https://gmedia.playstation.com/is/image/SIEPDC/dualsense-deep-earth-collection-background-block-desktop-01-en-11sep23?$1600px$`);
@@ -6,7 +6,7 @@ function Games({games}){
     let carouselTrack = useRef();
     let leftBtn = useRef();
     let rightBtn = useRef();
-    let currentSlide = useRef();
+    // let currentSlide = useRef();
 
     //Events
     function slideLeft(e){
@@ -29,7 +29,7 @@ function Games({games}){
             onClick={(e)=>{
                 setThumbNail(game.thumbnail);
             }}>
-                <img src={game.image}></img>
+                <img src={game.image} alt="NA"></img>
                 <div className="game-card-info">
                     <p>
                         <span>{game.name}</span>
@@ -43,7 +43,7 @@ function Games({games}){
         <>
             <img src={thumbNail} className="display-img" alt="NA"></img>
             <div className="carousel">
-                <img ref={leftBtn} src="https://cdn-icons-png.flaticon.com/128/3148/3148350.png" className="left-carousel-btn" onClick={slideRight}></img>
+                <img ref={leftBtn} src="https://cdn-icons-png.flaticon.com/128/3148/3148350.png" className="left-carousel-btn" onClick={slideRight} alt="LEFT"></img>
                 <div ref={carouselTrack} className="slides-container track">
                     {/* <div ref={currentSlide} className="slide active-slide">
                         <img src="https://media.gamestop.com/i/gamestop/20006017/Marvels-Spider-Man-2---PlayStation-5?$pdp2x$$&fmt=webp"></img>
@@ -51,7 +51,7 @@ function Games({games}){
                     {gamesDiv}
 
                 </div>
-                <img ref={rightBtn} src="https://cdn-icons-png.flaticon.com/128/3148/3148350.png" className="right-carousel-btn" onClick={slideLeft}></img>
+                <img ref={rightBtn} src="https://cdn-icons-png.flaticon.com/128/3148/3148350.png" className="right-carousel-btn" onClick={slideLeft} alt="RIGHT"></img>
             </div>
         </>
     );
