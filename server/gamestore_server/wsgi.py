0ca7501902c172b865e1ed9ff5d9a9e0c185ae9a
Gad-Ongoro <gadongoro1@gmail.com>
2024-05-25 22:43:03 +0300

    feat: Django Project Init

--- server/gamestore_server/wsgi.py
+++ server/gamestore_server/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Game_Store_Server project.
+WSGI config for gamestore_server project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Game_Store_Server.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamestore_server.settings')
 
 application = get_wsgi_application()
--- server/users/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('users/', views.Get_Users.as_view(), name='get_all_users'),
-    path('users/<uuid:user_id>/', views.UsersRetrieveUpdateDestroyAPIView.as_view(), name='user_RUD')
-]
