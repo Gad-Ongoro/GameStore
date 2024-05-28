6296bf47ac12f01361bd71aeeec95ecc50041e03
Gad-Ongoro <gadongoro1@gmail.com>
2024-05-28 17:27:57 +0300

    feat: Added models having the user model as an AbstractUser from django.contrib.auth.models

--- server/api/models.py
+++ server/api/models.py
@@ -1,10 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+# Abstract User Model
+class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(max_length=100, unique=True)
+    role = models.IntegerField(default=1000)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    # Related_name to avoid clashes with built-in User model
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='user_set',
+        related_query_name='user',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        verbose_name='groups',
+    )
+    
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='user_set',
+        related_query_name='user',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
+    
     def __str__(self):
         return self.username
     
@@ -19,7 +43,7 @@ class Product(models.Model):
         return self.title
     
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
@@ -27,7 +51,7 @@ class Cart(models.Model):
         return self.user.username
     
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     total_price = models.IntegerField()
