74e2a04109f62e52309ccc76a22e73cc10b1887f
Gad-Ongoro <gadongoro1@gmail.com>
2024-06-25 15:10:52 +0300

    feat: Registered models

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+admin.site.register(models.CustomUser, UserAdmin)
+admin.site.register(models.Profile)
+admin.site.register(models.ShippingAddress)
+admin.site.register(models.Product)
+admin.site.register(models.Cart)
+admin.site.register(models.Order)
+admin.site.register(models.Review)
+admin.site.register(models.ProductImage)
+admin.site.register(models.Payment)
