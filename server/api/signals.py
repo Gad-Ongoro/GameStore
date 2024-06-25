7f7a65a583480dba6cf24b5881b6cc5e2ae7bd89
Gad-Ongoro <gadongoro1@gmail.com>
2024-06-25 13:04:52 +0300

    feat: Django Signals to Create/Update Profile when user object is created

---
+++ server/api/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from . import models
+
+@receiver(post_save, sender=models.CustomUser)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        models.Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
